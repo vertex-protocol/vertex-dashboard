ea97a93286b98fc775ce1cff7eab291c32a87e95
josephl37 <josephliow2000@gmail.com>
2023-06-02 21:36:24 -0500

    add target blank to nav links

--- app/components/layout/NavBar.tsx
+++ app/components/layout/NavBar.tsx
@@ -6,8 +6,13 @@ export default function MoneyMarket() {
     <nav className="flex justify-between py-4 px-10 bg-gray-3 border-b border-gray-2 text-white">
       <Image src={logo} alt="logo" width={100} />
       <div className="flex gap-4">
-        <a href="https://vertexprotocol.com/">App</a>
-        <a href="https://vertex-protocol.gitbook.io/docs/getting-started/overview">
+        <a href="https://vertexprotocol.com/" target="_blank">
+          App
+        </a>
+        <a
+          href="https://vertex-protocol.gitbook.io/docs/getting-started/overview"
+          target="_blank"
+        >
           Docs
         </a>
       </div>
