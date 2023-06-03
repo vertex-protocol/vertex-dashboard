02ca38297ee66e80f305efb8ee8cfac2e3ea26a9
josephl37 <josephliow2000@gmail.com>
2023-06-02 21:31:31 -0500

    style card

--- app/(stats)/overview/page.tsx
+++ app/(stats)/overview/page.tsx
@@ -1,7 +1,12 @@
+import Card from '@/app/components/main/Card';
+
 export default function Overview() {
   return (
-    <main className="text-white">
-      <p>Overview</p>
+    <main className="py-6 grid md:grid-cols-4 grid-cols-1 gap-4">
+      <Card title="Total Trading Volume" stat={2.21} daily={14.08} />
+      <Card title="Trading Volume (24h)" stat={2.21} daily={14.08} />
+      <Card title="Users (24h)" stat={1023} daily={14.08} />
+      <Card title="Fees (24h)" stat={2.21} daily={14.08} />
     </main>
   );
 }
---
+++ app/components/main/Card.tsx
@@ -0,0 +1,25 @@
+// TODO: format values
+// 2 decimal points
+// K, M, B
+// conditionaly render $
+// conditionaly render green or red
+
+interface Card {
+  title: string;
+  stat: number;
+  daily: number;
+}
+
+export default function Card({ title, stat, daily }: Card) {
+  return (
+    <div className="bg-gray-3 border border-gray-2 rounded">
+      <div className="pl-6 py-6">
+        <div className="flex gap-4 items-center">
+          <p className="text-3xl text-white font-medium">${stat}B</p>
+          <p className="text-green">+${daily}M</p>
+        </div>
+        <p className="text-gray-1 font-medium mt-1">{title}</p>
+      </div>
+    </div>
+  );
+}
