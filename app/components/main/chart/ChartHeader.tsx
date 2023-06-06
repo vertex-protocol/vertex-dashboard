6374359550190506126368f6d59d50a4f76cdf08
josephl37 <josephliow2000@gmail.com>
2023-06-05 19:32:46 -0500

    add margin to chart header for mobile

--- app/components/main/chart/ChartHeader.tsx
+++ app/components/main/chart/ChartHeader.tsx
@@ -5,7 +5,7 @@ interface ChartHeaderComponent {
 
 export default function ChartHeader({ title, text }: ChartHeaderComponent) {
   return (
-    <div className=" text-white px-3 h-16">
+    <div className=" text-white px-3 h-16 mb-4">
       <p className="font-medium">{title}</p>
       <p className="text-gray-1 text-sm mt-1">{text}</p>
     </div>
