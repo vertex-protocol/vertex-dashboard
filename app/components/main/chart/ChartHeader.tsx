06069d6ae30c300050719a008d5743b453c29019
josephl37 <josephliow2000@gmail.com>
2023-06-05 19:09:12 -0500

    fix height bug on chart header

--- app/components/main/chart/ChartHeader.tsx
+++ app/components/main/chart/ChartHeader.tsx
@@ -1,11 +1,11 @@
 interface ChartHeaderComponent {
   title: string;
-  text: string;
+  text?: string;
 }
 
 export default function ChartHeader({ title, text }: ChartHeaderComponent) {
   return (
-    <div className=" text-white px-3 pb-4">
+    <div className=" text-white px-3 h-16">
       <p className="font-medium">{title}</p>
       <p className="text-gray-1 text-sm mt-1">{text}</p>
     </div>
