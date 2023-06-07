4d682f46392ebaaabdf76d0d4943a07c8164411e
josephl37 <josephliow2000@gmail.com>
2023-06-07 18:53:00 -0500

    add queryFundingRates

---
+++ app/hooks/queryFundingRates.ts
@@ -0,0 +1,11 @@
+// TODO: finish calculating funding rates
+export function queryFundingRates(data: any, type: string) {
+  const result: number[] = [];
+
+  if (type == 'hourly') {
+  } else if (type == 'daily') {
+  } else if (type == 'annual') {
+  }
+
+  return result;
+}
