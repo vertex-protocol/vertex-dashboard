a395613e25c35868027e6e74d72ff366b8770d8a
josephl37 <josephliow2000@gmail.com>
2023-06-07 23:54:17 -0500

    fetch data on spot page

--- app/(stats)/spot/page.tsx
+++ app/(stats)/spot/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from '../../components/main/Card';
 import IntervalTab from '../../components/main/IntervalTab';
 import ThreeGridLayout from '../../components/layout/ThreeGridLayout';
@@ -12,9 +12,82 @@ import ChartsLayout from '@/app/components/layout/ChartsLayout';
 import ChartContainer from '@/app/components/main/chart/ChartContainer';
 import ChartHeader from '@/app/components/main/chart/ChartHeader';
 import LineBarChart from '@/app/components/main/chart/LineBar_Chart';
+import { useAppSelector } from '@/app/redux/store';
+import { queryTime } from '@/app/hooks/queryTime';
+import { queryProduct } from '@/app/hooks/queryProduct';
+import { queryDaily } from '@/app/hooks/queryDaily';
 
 export default function Spot({ interval, setInterval }: IntervalProps) {
   const [market, setMarket] = useState('all-spot');
+
+  // Spot Trading Vol
+  const [SpotVol, setSpotVol] = useState<number[]>([]);
+  const [DailySpotVol, setDailySpotVol] = useState<number[]>([]);
+
+  // # of Spot Trades
+  const [SpotTrades, setSpotTrades] = useState<number[]>([]);
+  const [DailySpotTrades, setDailySpotTrades] = useState<number[]>([]);
+
+  const data = useAppSelector((state) => state.data.data);
+  const dates = queryTime(data);
+
+  useEffect(() => {
+    const fetchSpotData = () => {
+      if (!data) return; // Return early if data is not available
+
+      switch (market) {
+        case 'all-spot':
+          // Spot Trading Vol
+          const allSpotVol = queryProduct(data, 'cumulative_volumes', [1, 3]);
+          const allDailySpotVol = queryDaily(allSpotVol);
+          setSpotVol(allSpotVol);
+          setDailySpotVol(allDailySpotVol);
+
+          // # of Spot Trades
+          const allSpotTrades = queryProduct(data, 'cumulative_trades', [1, 3]);
+          const allDailySpotTrades = queryDaily(allSpotTrades);
+          setSpotTrades(allSpotTrades);
+          setDailySpotTrades(allDailySpotTrades);
+
+          break;
+
+        case 'btc-spot':
+          // BTC-Spot Tarding Vol
+          const BTCSpotVol = queryProduct(data, 'cumulative_volumes', [1]);
+          const DailyBTCSpotVol = queryDaily(BTCSpotVol);
+          setSpotVol(BTCSpotVol);
+          setDailySpotVol(DailyBTCSpotVol);
+
+          // # of BTC Spot Trades
+          const BTCSpotTrades = queryProduct(data, 'cumulative_trades', [1]);
+          const DailyBTCSpotTrades = queryDaily(BTCSpotTrades);
+          setSpotTrades(BTCSpotTrades);
+          setDailySpotTrades(DailyBTCSpotTrades);
+
+          break;
+
+        case 'eth-spot':
+          // ETH-Spot Trading Vol
+          const ETHSpotVol = queryProduct(data, 'cumulative_volumes', [3]);
+          const DailyETHSpotVol = queryDaily(ETHSpotVol);
+          setSpotVol(ETHSpotVol);
+          setDailySpotVol(DailyETHSpotVol);
+
+          // # of ETH Spot Trades
+          const ETHSpotTrades = queryProduct(data, 'cumulative_trades', [3]);
+          const DailyETHSpotTrades = queryDaily(ETHSpotTrades);
+          setSpotTrades(ETHSpotTrades);
+          setDailySpotTrades(DailyETHSpotTrades);
+
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    fetchSpotData();
+  }, [market, interval, data]);
 
   return (
     <>
@@ -52,14 +125,22 @@ export default function Spot({ interval, setInterval }: IntervalProps) {
             title="Spot Trading Volume"
             text="The daily vs cumulative spot trading volume on Vertex."
           />
-          <LineBarChart />
+          <LineBarChart
+            dates={dates}
+            cumulative={SpotVol}
+            daily={DailySpotVol}
+          />
         </ChartContainer>
         <ChartContainer>
           <ChartHeader
             title="# of Spot Trades"
             text="The number of spot trades over the set period."
           />
-          <LineBarChart />
+          <LineBarChart
+            dates={dates}
+            cumulative={SpotTrades}
+            daily={DailySpotTrades}
+          />
         </ChartContainer>
       </ChartsLayout>
     </>
