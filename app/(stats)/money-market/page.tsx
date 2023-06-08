0717046d33b14969c703e2d89f567bd0e92a902c
josephl37 <josephliow2000@gmail.com>
2023-06-07 19:05:42 -0500

    stub mm page

--- app/(stats)/money-market/page.tsx
+++ app/(stats)/money-market/page.tsx
@@ -8,6 +8,10 @@ import ControlsLayout from '../../components/layout/ControlsLayout';
 import MktDropdown from '../../components/main/MktDropdown';
 import { MarketValues } from './MarketValues';
 import { IntervalProps } from '@/app/types/IntervalProps';
+import ChartsLayout from '@/app/components/layout/ChartsLayout';
+import ChartContainer from '@/app/components/main/chart/ChartContainer';
+import ChartHeader from '@/app/components/main/chart/ChartHeader';
+import LineBarChart from '@/app/components/main/chart/LineBar_Chart';
 
 export default function MoneyMarket({ interval, setInterval }: IntervalProps) {
   const [market, setMarket] = useState('all-mkt');
@@ -27,6 +31,34 @@ export default function MoneyMarket({ interval, setInterval }: IntervalProps) {
         />
         <IntervalTab interval={interval} setInterval={setInterval} />
       </ControlsLayout>
+      <ChartsLayout>
+        <ChartContainer>
+          <ChartHeader
+            title="Deposit Rate"
+            text="The deposit rate over the set period."
+          />
+        </ChartContainer>
+        <ChartContainer>
+          <ChartHeader
+            title="Borrow Rate"
+            text="The borrow rate over the set period."
+          />
+        </ChartContainer>
+        <ChartContainer>
+          <ChartHeader
+            title="Deposits"
+            text="The daily vs cumulative deposits on Vertex."
+          />
+          <LineBarChart />
+        </ChartContainer>
+        <ChartContainer>
+          <ChartHeader
+            title="Borrows"
+            text="The daily vs cumulative borrows on Vertex."
+          />
+          <LineBarChart />
+        </ChartContainer>
+      </ChartsLayout>
     </>
   );
 }
