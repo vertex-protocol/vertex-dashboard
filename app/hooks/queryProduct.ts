3bfec661993bc84acf789ad4ddcb879b0e7faa59
josephl37 <josephliow2000@gmail.com>
2023-06-08 23:00:33 -0500

    made queryProduct hook dynamic

--- app/hooks/queryProduct.ts
+++ app/hooks/queryProduct.ts
@@ -3,13 +3,13 @@ export function queryProduct(data: any, type: string, productId: number[]) {
 
   data.forEach((obj: any) => {
     if (obj[type]) {
-      if (productId.length > 1) {
-        const sum = obj[type][productId[0]] + obj[type][productId[1]];
-        result.push(sum);
-      } else {
-        const value = obj[type][productId[0]];
-        result.push(value);
-      }
+      let sum = 0;
+
+      productId.forEach((id: number) => {
+        sum += obj[type][id];
+      });
+
+      result.push(sum);
     }
   });
 
