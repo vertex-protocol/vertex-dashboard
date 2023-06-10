bf2c578becd42ee3b7d148ee4db594d34a6148b7
josephl37 <josephliow2000@gmail.com>
2023-06-10 17:29:58 -0500

    redirect root to overview

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <main className="text-white">
-      <p>Vertex Dashboard</p>
-    </main>
-  );
+  redirect('/overview');
 }
