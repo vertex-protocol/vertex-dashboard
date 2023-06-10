8f4ff6732e5e5751f38811460e275de0d07e89e8
josephl37 <josephliow2000@gmail.com>
2023-06-10 17:06:00 -0500

    clean up spot page

--- app/(stats)/spot/page.tsx
+++ app/(stats)/spot/page.tsx
@@ -14,8 +14,7 @@ import ChartHeader from '@/app/components/main/chart/ChartHeader';
 import LineBarChart from '@/app/components/main/chart/LineBar_Chart';
 import { useAppSelector } from '@/app/redux/store';
 import { queryTime } from '@/app/hooks/queryTime';
-import { queryProduct } from '@/app/hooks/queryProduct';
-import { queryDaily } from '@/app/hooks/queryDaily';
+import { fetchSpotData } from '@/app/hooks/fetchSpotData';
 
 export default function Spot({ interval, setInterval }: IntervalProps) {
   const [market, setMarket] = useState('all-spot');
@@ -32,61 +31,14 @@ export default function Spot({ interval, setInterval }: IntervalProps) {
   const dates = queryTime(data);
 
   useEffect(() => {
-    const fetchSpotData = () => {
-      if (!data) return; // Return early if data is not available
-
-      switch (market) {
-        case 'all-spot':
-          // Spot Trading Vol
-          const allSpotVol = queryProduct(data, 'cumulative_volumes', [1, 3]);
-          const allDailySpotVol = queryDaily(allSpotVol);
-          setSpotVol(allSpotVol);
-          setDailySpotVol(allDailySpotVol);
-
-          // # of Spot Trades
-          const allSpotTrades = queryProduct(data, 'cumulative_trades', [1, 3]);
-          const allDailySpotTrades = queryDaily(allSpotTrades);
-          setSpotTrades(allSpotTrades);
-          setDailySpotTrades(allDailySpotTrades);
-
-          break;
-
-        case 'btc-spot':
-          // BTC-Spot Tarding Vol
-          const BTCSpotVol = queryProduct(data, 'cumulative_volumes', [1]);
-          const DailyBTCSpotVol = queryDaily(BTCSpotVol);
-          setSpotVol(BTCSpotVol);
-          setDailySpotVol(DailyBTCSpotVol);
-
-          // # of BTC Spot Trades
-          const BTCSpotTrades = queryProduct(data, 'cumulative_trades', [1]);
-          const DailyBTCSpotTrades = queryDaily(BTCSpotTrades);
-          setSpotTrades(BTCSpotTrades);
-          setDailySpotTrades(DailyBTCSpotTrades);
-
-          break;
-
-        case 'eth-spot':
-          // ETH-Spot Trading Vol
-          const ETHSpotVol = queryProduct(data, 'cumulative_volumes', [3]);
-          const DailyETHSpotVol = queryDaily(ETHSpotVol);
-          setSpotVol(ETHSpotVol);
-          setDailySpotVol(DailyETHSpotVol);
-
-          // # of ETH Spot Trades
-          const ETHSpotTrades = queryProduct(data, 'cumulative_trades', [3]);
-          const DailyETHSpotTrades = queryDaily(ETHSpotTrades);
-          setSpotTrades(ETHSpotTrades);
-          setDailySpotTrades(DailyETHSpotTrades);
-
-          break;
-
-        default:
-          break;
-      }
-    };
-
-    fetchSpotData();
+    fetchSpotData({
+      data,
+      market,
+      setSpotVol,
+      setDailySpotVol,
+      setSpotTrades,
+      setDailySpotTrades,
+    });
   }, [market, interval, data]);
 
   return (
