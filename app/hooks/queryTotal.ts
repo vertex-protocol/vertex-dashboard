b498ef30353587cfe598e0883c1ae1b32ca5af52
josephl37 <josephliow2000@gmail.com>
2023-07-06 16:02:25 -0500

    fix queryTotal

--- app/hooks/queryTotal.ts
+++ app/hooks/queryTotal.ts
@@ -1,11 +1,23 @@
+import BigNumber from 'bignumber.js';
+
 export function queryTotal(data: any, type: string) {
-  const result: number[] = [];
+  if (data === null || data === undefined) {
+    return []; // or handle the null case appropriately
+  }
+
+  const result: string[] = [];
 
   for (const key in data) {
     const obj = data[key];
-    const sum = obj[type].reduce((acc: number, val: number) => acc + val, 0);
-    result.push(sum);
+    obj.forEach((obj: any) => {
+      const sum = Object.values(obj[type] || {}).reduce(
+        (acc: number, val: unknown) => acc + parseInt(val as string),
+        0,
+      );
+      const formattedSum = new BigNumber(sum).dividedBy(1e18).toFixed(0);
+      result.push(formattedSum);
+    });
   }
 
-  return result;
+  return result.reverse();
 }
