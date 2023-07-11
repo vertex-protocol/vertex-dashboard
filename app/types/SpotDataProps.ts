c873efec8a7145b1a408bdb4e033b9706a2c39a1
josephl37 <josephliow2000@gmail.com>
2023-07-11 11:05:00 -0500

    add filteredProducts to SpotDataProps

--- app/types/SpotDataProps.ts
+++ app/types/SpotDataProps.ts
@@ -1,8 +1,9 @@
 export interface SpotDataProps {
-  data: any;
+  snapshotData: any;
   market: string;
   setSpotVol: React.Dispatch<React.SetStateAction<number[]>>;
   setDailySpotVol: React.Dispatch<React.SetStateAction<number[]>>;
   setSpotTrades: React.Dispatch<React.SetStateAction<number[]>>;
   setDailySpotTrades: React.Dispatch<React.SetStateAction<number[]>>;
+  filterdProducts: any;
 }
