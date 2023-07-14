71d0bb391594c0009f14f41d401e6f7df5729f7c
josephl37 <josephliow2000@gmail.com>
2023-07-14 10:12:51 -0500

    add restricted component

--- app/(stats)/layout.tsx
+++ app/(stats)/layout.tsx
@@ -12,14 +12,17 @@ import Overview from './overview/page';
 import Perpetual from './perpetual/page';
 import Spot from './spot/page';
 import MoneyMarket from './money-market/page';
+import Restricted from '../components/main/Restricted';
 import { fetchProducts } from '../redux/productsSlice';
 import { fetchPrices } from '../redux/pricesSlice';
+import { useAppSelector } from '../redux/store';
 
 export default function DashboardLayout() {
   const router = useRouter();
   const pathname = usePathname();
   const [interval, setInterval] = useState('31');
   const [path, setPath] = useState(pathname);
+  const error = useAppSelector((state) => state.data.error);
 
   const handleTabChange = (value: string) => {
     setPath(value);
@@ -43,29 +46,35 @@ export default function DashboardLayout() {
 
   return (
     <section>
-      <Nav />
-      <div className="px-10 mt-4">
-        <Tabs value={path} onValueChange={handleTabChange}>
-          <TabsList>
-            <TabsTrigger value="/overview">Overview</TabsTrigger>
-            <TabsTrigger value="/perpetual">Perpetual</TabsTrigger>
-            <TabsTrigger value="/spot">Spot</TabsTrigger>
-            <TabsTrigger value="/money-market">Money Market</TabsTrigger>
-          </TabsList>
-        </Tabs>
-        {path === '/overview' && (
-          <Overview interval={interval} setInterval={setInterval} />
-        )}
-        {path === '/perpetual' && (
-          <Perpetual interval={interval} setInterval={setInterval} />
-        )}
-        {path === '/spot' && (
-          <Spot interval={interval} setInterval={setInterval} />
-        )}
-        {path === '/money-market' && (
-          <MoneyMarket interval={interval} setInterval={setInterval} />
-        )}
-      </div>
+      {error ? (
+        <Restricted />
+      ) : (
+        <>
+          <Nav />
+          <div className="px-10 mt-4">
+            <Tabs value={path} onValueChange={handleTabChange}>
+              <TabsList>
+                <TabsTrigger value="/overview">Overview</TabsTrigger>
+                <TabsTrigger value="/perpetual">Perpetual</TabsTrigger>
+                <TabsTrigger value="/spot">Spot</TabsTrigger>
+                <TabsTrigger value="/money-market">Money Market</TabsTrigger>
+              </TabsList>
+            </Tabs>
+            {path === '/overview' && (
+              <Overview interval={interval} setInterval={setInterval} />
+            )}
+            {path === '/perpetual' && (
+              <Perpetual interval={interval} setInterval={setInterval} />
+            )}
+            {path === '/spot' && (
+              <Spot interval={interval} setInterval={setInterval} />
+            )}
+            {path === '/money-market' && (
+              <MoneyMarket interval={interval} setInterval={setInterval} />
+            )}
+          </div>
+        </>
+      )}
     </section>
   );
 }
---
+++ app/components/main/Restricted.tsx
@@ -0,0 +1,47 @@
+export default function Restricted() {
+  return (
+    <div className="flex mt-40 justify-center">
+      <div className="bg-gray-3 border border-gray-2 w-96 h-auto">
+        <div className="text-white font-semibold text-xl border-b border-gray-2 px-4 py-2">
+          <p>Restricted Territory</p>
+        </div>
+        <div className="px-4 mt-4">
+          <p className="text-gray-1 text-xs">
+            It appears that you are accessing Vertex from a Restricted
+            Territory. Unfortunately, we are not able to support users from the
+            following Restricted Territories at this time:
+          </p>
+        </div>
+        <div className="text-gray-1 text-xs mx-4 my-4 border border-gray-2">
+          <div className="p-2">
+            <p>Belarus</p>
+            <p>Cuba</p>
+            <p>Iran</p>
+            <p>North Korea</p>
+            <p>Russia</p>
+            <p>Syria</p>
+            <p>
+              Ukraine (Crimea, Donetsk, Kherson, Luhansk, and Zaporizhzhia
+              regions)
+            </p>
+            <p>United States of America</p>
+          </div>
+        </div>
+        <div className="px-4 my-4">
+          <p className="text-gray-1 text-xs">
+            Please refer to our{' '}
+            <a
+              href="https://vertexprotocol.com/docs/TermsOfUse.pdf"
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+            >
+              Terms of Use
+            </a>{' '}
+            for additional information.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
