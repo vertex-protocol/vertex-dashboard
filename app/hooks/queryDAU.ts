0a9a3138be8644a3564ca2eacc24bb6335363088
josephl37 <josephliow2000@gmail.com>
2023-07-14 09:39:00 -0500

    add queryDAU hook

--- app/hooks/queryDAU.ts
+++ app/hooks/queryDAU.ts
@@ -2,5 +2,15 @@ export function queryDAU(data: any) {
   if (!data) {
     return 0;
   }
-  return data.snapshots[0].daily_active_users;
+  const users: any = [];
+
+  data?.snapshots.forEach((obj: any) => {
+    if (obj?.hasOwnProperty('daily_active_users')) {
+      const user = obj.daily_active_users;
+      users.push(user);
+    }
+  });
+
+  users.pop();
+  return users.reverse();
 }
