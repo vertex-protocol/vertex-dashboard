da80e793530613721d7d6b2c13e05bb4d04d4b94
josephl37 <josephliow2000@gmail.com>
2023-12-19 14:01:00 -0600

    temp remove 90D and ALL interval

--- app/components/main/IntervalDropdown.tsx
+++ app/components/main/IntervalDropdown.tsx
@@ -27,8 +27,9 @@ export default function IntervalDropdown({
       <SelectContent className="bg-gray-3 border border-gray-2 rounded text-gray-1 text-sm focus:outline-none w-20 mt-2 cursor-pointer">
         <SelectItem value="8">7D</SelectItem>
         <SelectItem value="31">30D</SelectItem>
-        <SelectItem value="91">90D</SelectItem>
-        <SelectItem value="all">ALL</SelectItem>
+        {/* Temporarily removing 90D & ALL interval so max count limits can be revert, will add back when dashboard is migrated to monorepo */}
+        {/* <SelectItem value="91">90D</SelectItem>
+        <SelectItem value="all">ALL</SelectItem> */}
       </SelectContent>
     </Select>
   );
