20c966a93b4de8103eecee90ceb9d835b1e30fee
josephl37 <josephliow2000@gmail.com>
2024-05-01 15:28:28 -0500

    specify arbitrum

--- app/components/main/NavBar.tsx
+++ app/components/main/NavBar.tsx
@@ -3,10 +3,13 @@ import vertexLogo from '../../public/vertex-logo.svg';
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-between py-4 px-10 bg-gray-3 border-b border-gray-2 text-white">
-      <a href="https://vertexprotocol.com" target="_blank">
-        <Image src={vertexLogo} alt="vertex-logo" width={100} />
-      </a>
+    <nav className="flex justify-between py-4 px-4 md:px-10 bg-gray-3 border-b border-gray-2 text-white">
+      <div className="flex gap-x-2">
+        <a href="https://vertexprotocol.com" target="_blank">
+          <Image src={vertexLogo} alt="vertex-logo" width={100} />
+        </a>
+        <span>on Arbitrum</span>
+      </div>
       <div className="flex gap-4">
         <a href="https://app.vertexprotocol.com/markets" target="_blank">
           App
