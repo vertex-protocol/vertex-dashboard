c47c097c75f2205205744bd692e829208f47070a
josephl37 <josephliow2000@gmail.com>
2024-05-03 14:23:21 -0500

    fix padding on stats page

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Dashboard() {
       ) : (
         <>
           <Nav />
-          <div className={`px-${isMobile ? '4' : '10'} mt-4`}>
+          <div className="px-4 md:px-10 mt-4">
             <Tabs value={selectedTab} onValueChange={setSelectedTab}>
               <TabsList>
                 <TabsTrigger
