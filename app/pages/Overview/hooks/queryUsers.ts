4324d26cdc630ce485b3f06483de3a42568863f5
josephl37 <josephliow2000@gmail.com>
2024-08-19 16:13:56 -0500

    fix query user bug

--- app/pages/Overview/hooks/queryUsers.ts
+++ app/pages/Overview/hooks/queryUsers.ts
@@ -1,12 +1,9 @@
-import StartData from '../../../data/StartData.json';
-
 export function queryUsers(data: any) {
   const users: any = [];
-  const startUser = StartData.cumulative_users;
 
   data?.forEach((obj: any) => {
     if (obj?.hasOwnProperty('cumulative_users')) {
-      let user = obj.cumulative_users - startUser;
+      let user = obj.cumulative_users;
       if (user < 0) {
         user = 0;
       }
